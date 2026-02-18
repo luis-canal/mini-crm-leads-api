31c0d4ef328ddfdd2484f3a5af1e0ce77927a266
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-02-18 15:44:50 -0300

    Correção dos tratamentos de erros

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI, Request
 from routes.leads import router
 from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+app.include_router(router)
+
 @app.exception_handler(Exception)
 def global_exception_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=500,
         content={"error": "Erro interno do servidor"}
     )
-
-app = FastAPI()
-
-app.include_router(router)
 
 @app.get("/")
 def home():
