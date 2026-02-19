f22b2dae4e071b2f265962d9c4d55a0808707477
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-02-19 10:48:49 -0300

    Atualizado e validado os testes (test_leads.py e test_health.py)

--- tests/test_leads.py
+++ tests/test_leads.py
@@ -2,8 +2,12 @@ import requests
 
 BASE_URL = "http://127.0.0.1:8000"
 
+headers = {
+    "x-api-key": "123456"
+}
 
 def test_criar_lead():
+
     response = requests.post(
         f"{BASE_URL}/leads",
         json={
@@ -11,19 +15,34 @@ def test_criar_lead():
             "telefone": "999999999",
             "carro": "Civic",
             "status": "novo"
-        }
+        },
+        headers=headers
     )
+
+    print(response.text)  # debug opcional
 
     assert response.status_code == 200
 
 
 def test_listar_leads():
-    response = requests.get(f"{BASE_URL}/leads")
+
+    response = requests.get(
+        f"{BASE_URL}/leads",
+        headers=headers
+    )
+
+    print(response.text)
 
     assert response.status_code == 200
 
 
-def test_estatisticas():
-    response = requests.get(f"{BASE_URL}/leads/stats")
+def test_stats():
+
+    response = requests.get(
+        f"{BASE_URL}/leads/stats",
+        headers=headers
+    )
+
+    print(response.text)
 
     assert response.status_code == 200
